81a729a8a2999575fe0cd1e0a659706b5d729b49
Sinan Yildiz <sinanyildiz@sinans-mbp.home>
2025-11-06 19:38:29 -0500

    Collection postman et swagger qui fonctionne

--- src/V2/controllers/authController.ts
+++ src/V2/controllers/authController.ts
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken'
 import configPkg from 'config'
 import User, { IUser } from '../models/User.js'
 import { emailContrainte, userNameContrainte } from '../utils/validators.js'
-
+ 
 const config = configPkg as any
 //Verifier si le password est fort
 function isStrongPassword(pw: string): boolean {
@@ -21,32 +21,32 @@ function isStrongPassword(pw: string): boolean {
 //Pour cree un nouveau utilisateur apres avoir valider les donnees
 export async function register(req: Request, res: Response) {
     try {
-        const { email, username, password } = req.body || {}
-
+        const { email, username, password, role } = req.body || {}
+ 
         if (!emailContrainte.test(email || '')) {
             return res.status(400).json({ message: 'Email est invalide' })
         }
-
+ 
         if (!userNameContrainte.test(username || '')) {
             return res.status(400).json({ message: 'Username est invalide' })
         }
-
+ 
         if (!isStrongPassword(password)) {
             return res.status(400).json({ message: 'Le password est faible' })
         }
-
+ 
         const exists = await User.findOne({ email })
         if (exists) {
             return res.status(409).json({ message: 'Email deja utiliser' })
         }
-
+ 
         //Juste pour hasher le mdp
         const passwordHash = await bcrypt.hash(password, 10)
-        const user = await User.create({ email, username, passwordHash, role: 'user' })
-
+        const user = await User.create({ email, username, passwordHash, role })
+ 
         const obj = user.toObject()
         delete (obj as any).passwordHash
-
+ 
         return res.status(201).json(obj)
     } catch (err) {
         return res.status(500).json({ message: 'Server erreur' })
@@ -57,22 +57,22 @@ export async function login(req: Request, res: Response) {
     try {
         const { email, password } = req.body || {}
         const user = await User.findOne({ email })
-
-        if (!user) {   
+ 
+        if (!user) {  
             return res.status(401).json({ message: 'Invalide credentials' })
         }
-
+ 
         const ok = await bcrypt.compare(password || '', user.passwordHash)
         if (!ok) {
             return res.status(401).json({ message: 'Invalide credentials' })
         }
-
+ 
         const id = (user as IUser)._id?.toString?.() ?? String((user as any)._id)
-
+ 
         const secret = config.get('security.jwt.secret')
         const expiresIn = config.get('security.jwt.expiresIn')
         const token = jwt.sign({ id, role: user.role }, secret, { expiresIn })
-
+ 
         return res.json({ token })
     } catch (err) {
         return res.status(500).json({ message: 'Server erreur' })
